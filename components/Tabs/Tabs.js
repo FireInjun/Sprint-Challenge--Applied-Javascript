89eccb972a8d8c4d94b53223ac7c46c9a3aeca88
FireInjun <mk@FireInjun.com>
2018-11-01 18:49:42 -0400

    11/1/2018 6:49 PM

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -21,7 +21,7 @@ class TabLink {
     this.parent = parent;
 
     // Note that we are calling getCards() on Tabs (The parent of TabLink) and passing the data attribute: data-tab, no need to update this line of code.
-    this.cards = this.parent.getCards(this.element.data('tab'));
+    this.cards = this.parent.getCards(this.element.data("tab"));
 
     // Using jQuery, map over the array of cards.  In your callback function, create new instances of the TabCard class that contain a card reference. TabCard(card) as an example.
     this.cards = this.cards.map((index, card) => {
@@ -30,73 +30,73 @@ class TabLink {
 
     // You will need to create a click handler for the TabLink element that calls selectTab()
     this.element.click(() => {
-      console.log("click")  
+      console.log("click");
       // this.Tabs.updateActive(this);
-        this.selectTab();
-      });
-    };
-
-    selectTab() {
-      // use this.parent to call the updateActive() method and pass the `this` keyword as a parameter
-      this.parent.updateActive(this);
-      // using a jQuery method, add a class to this.element named "active-tab"
-      this.element.addClass("active-tab");
-      // iterate over each card using the .each() method in jQuery. call the selectCard() method in your callback function
-      this.cards.each((index, card)=>{
-        card.selectCard();
-      });
-    }
-
-    deselectTab() {
-      // use a jQuery method to remove the class "active-tab" from this.element
-      this.element.removeClass("active-tab");
-      // iterate over each card using the .each() method in jQuery. call the deselectCard() method in your callback function
-      this.cards.each((index, card)=>{
-        card.deselectCard()
-      });
-    }
+      this.selectTab();
+    });
   }
 
-  class Tabs {
-    constructor(element) {
-      this.element = $(element);
+  selectTab() {
+    // use this.parent to call the updateActive() method and pass the `this` keyword as a parameter
+    this.parent.updateActive(this);
+    // using a jQuery method, add a class to this.element named "active-tab"
+    this.element.addClass("active-tab");
+    // iterate over each card using the .each() method in jQuery. call the selectCard() method in your callback function
+    this.cards.each((index, card) => {
+      card.selectCard();
+    });
+  }
 
-      // Using jQuery, find all of the tabs on this element.
-      this.tabs = this.element.find('.tab');
+  deselectTab() {
+    // use a jQuery method to remove the class "active-tab" from this.element
+    this.element.removeClass("active-tab");
+    // iterate over each card using the .each() method in jQuery. call the deselectCard() method in your callback function
+    this.cards.each((index, card) => {
+      card.deselectCard();
+    });
+  }
+}
 
-      this.tabs = this.tabs.map((i, tab) => new TabLink(tab, this));
+class Tabs {
+  constructor(element) {
+    this.element = $(element);
 
-      // Set the initial active tab to the first tab in the list.
-      this.activeTab = this.tabs[0];
+    // Using jQuery, find all of the tabs on this element.
+    this.tabs = this.element.find(".tab");
 
-      this.init();
-    }
+    this.tabs = this.tabs.map((i, tab) => new TabLink(tab, this));
 
-    init() {
-      // use activeTab to call the selectTab() method
-      this.activeTab.selectTab();
-    }
+    // Set the initial active tab to the first tab in the list.
+    this.activeTab = this.tabs[0];
 
-    updateActive(tabElement) {
-      // use activeTab to call the deselectTab() method
-      this.activeTab.deselectTab();
-      // assign activeTab to tabElement
-      this.activeTab = tabElement;
-    }
+    this.init();
+  }
 
-    getCards(data) {
-      // This method is meant to get all the cards from the HTML page.  
-      // If the data supplied is 'all' then all of the cards should be returned. 
-      // Otherwise, only cards matching the data attribute should be returned. 
-    if(data === "all"){
-      return $('.card')
+  init() {
+    // use activeTab to call the selectTab() method
+    this.activeTab.selectTab();
+  }
+
+  updateActive(tabElement) {
+    // use activeTab to call the deselectTab() method
+    this.activeTab.deselectTab();
+    // assign activeTab to tabElement
+    this.activeTab = tabElement;
+  }
+
+  getCards(data) {
+    // This method is meant to get all the cards from the HTML page.
+    // If the data supplied is 'all' then all of the cards should be returned.
+    // Otherwise, only cards matching the data attribute should be returned.
+    if (data === "all") {
+      return $(".card");
     } else {
       return $(`.card[data-tab=${data}]`);
     }
     // $(`.card[data-tab=${data}]`);
-    }
   }
+}
 
-  // Using jQuery, select the correct tabs component. Then initialize the Tabs class.
-  let tabs = $(".tabs");
-  tabs = new Tabs(tabs);
+// Using jQuery, select the correct tabs component. Then initialize the Tabs class.
+let tabs = $(".tabs");
+tabs = new Tabs(tabs);
--- tempWorkSP/referenceCode.js
+++ tempWorkSP/referenceCode.js
@@ -1,127 +1,121 @@
 class TabsItem {
-    constructor($element) {
-        // Attach dom element to object. Example in Tabs class
-        this.element = $element;
-    }
+  constructor($element) {
+    // Attach dom element to object. Example in Tabs class
+    this.element = $element;
+  }
 
-    select() {
-        // Selects the item by adding a class
-        /* Stretch goal: use a built in jQuery method to show the item */
-        this.element.addClass("tabs-item-selected");
-    }
+  select() {
+    // Selects the item by adding a class
+    /* Stretch goal: use a built in jQuery method to show the item */
+    this.element.addClass("tabs-item-selected");
+  }
 
-    deselect() {
-        // Deselects the item by removing a class
-        /* Stretch goal: use a built in jQuery method to hide the item */
-        this.element.removeClass("tabs-item-selected")
-    }
+  deselect() {
+    // Deselects the item by removing a class
+    /* Stretch goal: use a built in jQuery method to hide the item */
+    this.element.removeClass("tabs-item-selected");
+  }
 }
 
 class TabsLink {
-    constructor($element, parent) {
-        // Attach the element to this instance of the TabsLink class
-        this.element = $element;
-        // Attach Tabs (parent) to this instance of the TabsLink class
-        this.tabs = parent;
-        /* Use the getTab method on the parent to find the corresponding TabItem for this link
+  constructor($element, parent) {
+    // Attach the element to this instance of the TabsLink class
+    this.element = $element;
+    // Attach Tabs (parent) to this instance of the TabsLink class
+    this.tabs = parent;
+    /* Use the getTab method on the parent to find the corresponding TabItem for this link
            hint: use the data-tab attribute */
-        //To DO
-        this.tabsItem = parent.getTab(this.element.data("tab"));
-        // Reassign this.tabsItem to be a new instance of TabsItem, passing it this.tabsItem
-        this.tabsItem = new TabsItem($(this.tabsItem));
-        /* Add an click event to the main element, this will update the active tab on the parent, 
+    //To DO
+    this.tabsItem = parent.getTab(this.element.data("tab"));
+    // Reassign this.tabsItem to be a new instance of TabsItem, passing it this.tabsItem
+    this.tabsItem = new TabsItem($(this.tabsItem));
+    /* Add an click event to the main element, this will update the active tab on the parent, 
            and should call select on this tab */
-        this.element.click(() => {
-            this.tabs.updateActive(this);
-            this.select();
-        });
-    };
+    this.element.click(() => {
+      this.tabs.updateActive(this);
+      this.select();
+    });
+  }
 
-    select() {
-        // add selected class to this link
-        // select the associated tab item
-        this.element.addClass("tabs-link-selected");
-        this.tabsItem.select()
-    }
+  select() {
+    // add selected class to this link
+    // select the associated tab item
+    this.element.addClass("tabs-link-selected");
+    this.tabsItem.select();
+  }
 
-    deselect() {
-        // deselect this link
-        // deselect the associated tab item
-        this.element.removeClass("tabs-link-selected");
-        this.tabsItem.deselect()
-    }
+  deselect() {
+    // deselect this link
+    // deselect the associated tab item
+    this.element.removeClass("tabs-link-selected");
+    this.tabsItem.deselect();
+  }
 }
 
 class Tabs {
-    constructor($element) {
-        this.element = $element;
+  constructor($element) {
+    this.element = $element;
 
-        // Using jQuery's .find method, get an array of all links on the element
-        this.links = $(".tabs-link")
+    // Using jQuery's .find method, get an array of all links on the element
+    this.links = $(".tabs-link");
 
-        // This step will map over the array creating new TabsLink class isntances of each link.
-        this.links = this.links.map((index, link) => {
-            return new TabsLink($(link), this);
-        });
+    // This step will map over the array creating new TabsLink class isntances of each link.
+    this.links = this.links.map((index, link) => {
+      return new TabsLink($(link), this);
+    });
 
-        // Select the first Link and set it to the activeLink
-        this.activeLink = this.links[0];
-        this.init();
-    }
+    // Select the first Link and set it to the activeLink
+    this.activeLink = this.links[0];
+    this.init();
+  }
 
-    init() {
-        // Select the first link and tab upon ititialization
-        this.activeLink.select();
-    }
+  init() {
+    // Select the first link and tab upon ititialization
+    this.activeLink.select();
+  }
 
-    updateActive(newActive) {
-        // Deselect the old active link
-        this.activeLink.deselect();
-        // Assign the new active link
-        this.activeLink = newActive;
+  updateActive(newActive) {
+    // Deselect the old active link
+    this.activeLink.deselect();
+    // Assign the new active link
+    this.activeLink = newActive;
+  }
 
-    }
-
-    getTab(data) {
-        // Use the tab item classname and the data attribute to select the proper item
-        // return $(`.tabs-item[data-tab="${data}"]`);
-        return this.element.find(`.tabs-item[data-tab="${data}"]`);
-    }
-
+  getTab(data) {
+    // Use the tab item classname and the data attribute to select the proper item
+    // return $(`.tabs-item[data-tab="${data}"]`);
+    return this.element.find(`.tabs-item[data-tab="${data}"]`);
+  }
 }
 
 /* Using jQuery, select all instances of the class tabs, map over it and create new instances 
    of the Tabs class with the element */
 let tabs = $(".tabs");
 tabs = tabs.map((index, tabs) => {
-    return new Tabs($(tabs));
+  return new Tabs($(tabs));
 });
 
 class Dropdown {
-    constructor(element) {
-        this.element = $(element); // 
-        // Select button and content from the element
-        this.button = this.element;
-        this.content = this.element;
-        // Add a click handler to the button
-        this.button.click(() => {
-            this.toggleContent();
-        });
-    }
+  constructor(element) {
+    this.element = $(element); //
+    // Select button and content from the element
+    this.button = this.element;
+    this.content = this.element;
+    // Add a click handler to the button
+    this.button.click(() => {
+      this.toggleContent();
+    });
+  }
 
-    toggleContent() {
-        // Add/remove class to/from content
-        let dropdownContent = $(".dropdown-content");
-        dropdownContent.toggleClass("dropdown-hidden");
-    }
-
+  toggleContent() {
+    // Add/remove class to/from content
+    let dropdownContent = $(".dropdown-content");
+    dropdownContent.toggleClass("dropdown-hidden");
+  }
 }
 
-let $dropdown = $('.dropdown');
+let $dropdown = $(".dropdown");
 $dropdown = new Dropdown($dropdown);
-
-
-
 
 // class Dropdown {
 //   constructor($element) {
